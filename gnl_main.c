bc9d4d6a60433d8365475f2792b667b47fdb179c
Gnmsk <gennadyartemev@gmail.com>
2019-04-08 10:15:56 +0300

    3

--- gnl_main.c
+++ gnl_main.c
@@ -1,8 +1,16 @@
+//The return value can be 1, 0 or -1
+//depending on whether a line has been read,
+//when the reading has been completed, 
+//or if an error has happened respectively.
+
 #include "get_next_line.h"
 
-char	*find_line()
+char	*find_line(char *buffer_for_check)
 {
-	return();
+	if (ft_strchr(buffer_for_check, '\n') == NULL)
+		return(NULL);
+	else
+
 }
 
 static	gnl_list	*find_list(int fd, gnl_list **mem_list)
@@ -34,19 +42,27 @@ int	get_next_line(int fd, char **line)
 {
         char    	buffer[BUFF_SIZE + 1];
 	static gnl_list	*memory_lists;
+	int i;
 
+	i = 0;
 	if (fd < 0 || !line || read(fd, buffer, BUFF_SIZE) < 0)
 		return (-1);
 	buffer[BUFF_SIZE] = '\0';
-	ft_strccpy(*line, buffer, '\n');//tempary!!!!!
-	if (find_list(fd, &memory_lists)->data)
+//	ft_strccpy(*line, buffer, '\n');
+	if (find_line(find_list(fd, &memory_lists->data)) == NULL)
 	{
-		
+		while(find_line(find_list(fd, &memory_lists->data)))
+		{
+			ft_strrejoin(&memory_lists->data, buffer);
+			find_line(memory_lists->data);
+			read(fd, buffer, BUFF_SIZE);
+			buffer[BUFF_SIZE]= '\0';
+		}
 	}
-	else
-	{
-
-	}
+	if ()
+-		;
+-
+-
 }
 
 int	main(int argc, char **argv)
